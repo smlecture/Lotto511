aa23e4aa9c7866738dfda10190930fa0c15caca6
H-J Yoo <yoohj@smu.ac.kr>
2021-05-19 00:27:40 +0900

    Intents

--- app/src/main/java/com/androidapp/lotto511/MainActivity.kt
+++ app/src/main/java/com/androidapp/lotto511/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.androidapp.lotto511
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val cardRandom = findViewById<CardView>(R.id.cardRandom)
+        val cardConstellation = findViewById<CardView>(R.id.cardConstellation)
+        val cardName = findViewById<CardView>(R.id.cardName)
+
+        cardRandom.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ResultActivity::class.java))
+        }
+        cardConstellation.setOnClickListener {
+            startActivity(Intent(this@MainActivity, ConstellationActivity::class.java))
+        }
+        cardName.setOnClickListener {
+            startActivity(Intent(this@MainActivity, NameActivity::class.java))
+        }
     }
 }
--- app/src/main/java/com/androidapp/lotto511/NameActivity.kt
+++ app/src/main/java/com/androidapp/lotto511/NameActivity.kt
@@ -1,11 +1,23 @@
 package com.androidapp.lotto511
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class NameActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_name)
+
+        val btnGoResult = findViewById<Button>(R.id.btnGoResult)
+        val btnBack = findViewById<Button>(R.id.btnBack)
+
+        btnGoResult.setOnClickListener {
+            startActivity(Intent(this, ResultActivity::class.java))
+        }
+        btnBack.setOnClickListener {
+            finish()
+        }
     }
 }
